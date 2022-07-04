771bb585918b4924c47653550bf2d7516502c503
ninimama <nimasarajpoor@gmail.com>
2022-07-03 22:36:07 -0600

    initialize with files

---
+++ LeetCode/two_sum.py
@@ -0,0 +1,31 @@
+# link: https://leetcode.com/problems/two-sum/
+
+def two_sum(nums, target):
+    """
+    this function gets a list of integers `nums` and return indices of two
+    elementst that they add up to `target`
+
+    assumption: each input has exactly one solution, and there is no repeated value
+    constraints: please see https://leetcode.com/problems/two-sum/
+
+    Parameters
+    ----------
+    nums: list
+        list of integers, with minimum length of two.
+
+    target: int
+        the number that is the summation of two elements from `nums`
+
+    Returns
+    -------
+    out: list
+        a list of length two that contains the indices of elements from `nums`
+        whose sum is `target`
+    """
+
+    positions = {}
+    for i, val in enumrate(nums):
+        if target - val in positions.keys():
+            return [positions[target - val], i]
+
+        positions[val] = i
---
+++ LeetCode_tests/test_two_sum.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def test_two_sum():
+    a = np.random.rand(1000)
+    for m in range(2, 100):
+        nums = np.random.choice(a, size=m, replace=False)
+
+        IDX = np.random.choice(np.arange(m), size=2, replace=False)
+        target = np.random.randint(10000, high=20000)
+        nums[IDX[1]] = target - nums[IDX[0]]
+
+        comp = two_sums(nums, target)
+
+        ref = np.sort(IDX).tolist()
+        comp = sorted(comp)
+
+        assert ref == comp
