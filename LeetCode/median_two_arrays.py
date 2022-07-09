39338bb7e7f352ed000d5c41698e6cd4b947ddc8
ninimama <nimasarajpoor@gmail.com>
2022-07-09 09:12:08 -0600

    Add function and unit test

---
+++ LeetCode/median_two_arrays.py
@@ -0,0 +1,56 @@
+import math
+def get_median_two_sorted_arrays(nums1, nums2):
+    """
+    find the median of two sorted arrays nums1 and nums2 after being merged into
+    one array
+
+    Parameters
+    ----------
+    nums1: List
+        a list of integer numbers sorted  in ascending order
+
+    nums2: List
+        a list of integer numbers sorted  in ascending order
+
+    Returns
+    -------
+    out: int
+        the median of the merge of `nums1` and `nums2`
+
+
+    Examples:
+    nums1 = [1, 3, 10]
+    nums2 = [4, 8]
+    >>> get_median_two_sorted_arrays(nums1, nums2)
+    4
+    """
+    dummy_lst = [float('-inf'), float('inf')]
+    if len(nums1) == 0:
+        nums1 = dummy_lst
+    if len(nums2) == 0:
+        nums2 = dummy_lst
+
+
+    n = len(nums1) + len(nums2)
+    n_itr = math.floor(n / 2 + 1)
+    # 1 2 3
+    # -np.inf, np.inf
+    i = 0
+    j = 0
+    current = float('-inf')
+    for _ in range(n_itr):
+        if nums1[i] <= nums2[j]:
+            prev = current
+            current = nums1[i]
+            i = i + 1
+        else:
+            prev = current
+            current = nums2[j]
+            j = j + 1
+
+    if n % 2:
+        median = (prev + current) / 2
+    else:
+        median = current
+
+    return median
---
+++ LeetCode/test_median_two_arrays.py
@@ -0,0 +1,54 @@
+from median_two_arrays import get_median_two_sorted_arrays
+
+def test_OneListEmpty():
+    nums1 = []
+    nums2 = [10, 20, 30]
+
+    ref = 20
+    assert get_median_two_sorted_arrays(nums1, nums2) == ref
+    assert get_median_two_sorted_arrays(nums2, nums1) == ref
+
+
+def test_SingleElement():
+    nums1 = [5]
+    nums2 = []
+
+    ref = 5
+    assert get_median_two_sorted_arrays(nums1, nums2) == ref
+    assert get_median_two_sorted_arrays(nums2, nums1) == ref
+
+
+def test_BothEven():
+    nums1 = [4, 7, 8, 10]
+    nums2 = [1, 3, 5, 6]
+
+    ref = 5.5
+    assert get_median_two_sorted_arrays(nums1, nums2) == ref
+    assert get_median_two_sorted_arrays(nums2, nums1) == ref
+
+
+def test_BothOdd():
+    nums1 = [4, 7, 8]
+    nums2 = [1, 3, 5]
+
+    ref = 4.5
+    assert get_median_two_sorted_arrays(nums1, nums2) == ref
+    assert get_median_two_sorted_arrays(nums2, nums1) == ref
+
+
+def test_EvenOdd():
+    nums1 = [7, 9]
+    nums2 = [1, 3, 8]
+
+    ref = 7
+    assert get_median_two_sorted_arrays(nums1, nums2) == ref
+    assert get_median_two_sorted_arrays(nums2, nums1) == ref
+
+
+def test_OneListAllSmall():
+    nums1 = [1, 2, 3, 4, 5]
+    nums2 = [6, 7, 8, 9, 10]
+
+    ref = 5.5
+    assert get_median_two_sorted_arrays(nums1, nums2) == ref
+    assert get_median_two_sorted_arrays(nums2, nums1) == ref
