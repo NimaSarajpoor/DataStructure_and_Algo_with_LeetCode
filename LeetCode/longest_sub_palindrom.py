af3a2e787844e1ff79fde9be1c4e704b641cfd77
ninimama <nimasarajpoor@gmail.com>
2022-07-09 17:41:01 -0600

    minor change in function and test function

--- LeetCode/longest_sub_palindrom.py
+++ LeetCode/longest_sub_palindrom.py
@@ -1,6 +1,6 @@
 # link: https://leetcode.com/problems/longest-palindromic-substring/
 
-def longest_sub_palindrom(s):
+def naive_longest_sub_palindrom(s):
     """
     finds the longest substring with palindromic property "NIMAMIN"
 
@@ -25,20 +25,73 @@ def longest_sub_palindrom(s):
     # abb --> bb
     # aab --> aa
     # aaa --> aaa
-    # aba --> aba
+    # aba --> abas
     # abccbcddddc --> cddddc
 
     if len(s) == 1:
         return s
 
-    out = s[0]
+    out = [0, 1] # start, stop
     max_length = 1
     for i in range(len(s)-1):
+        if len(s) - i <= max_length:
+            break
         for j in range(i + 1, len(s) + 1):
-            if abs(i - j) <= max_length:
+            if j - i <= max_length:
                 continue
             if s[i:j] == s[i:j][::-1]:
-                out = s[i:j]
-                max_length = len(out)
+                out = [i, j]
+                max_length = j - i
 
-    return out
+    return s[out[0]:out[1]]
+
+
+def naive_longest_sub_palindrom_enhanced(s):
+    """
+    This is the enhanced version of naive implementation
+
+    Finds the longest substring with palindromic property "NIMAMIN"
+
+    Parameters
+    ----------
+    s: str
+        A string with digits and english letters
+
+    Returns
+    -------
+    out: str
+        A string that is the longest substring of `s` and it is palindrom
+
+    Example:
+    s = "Goozoo"
+    >>> longest_sub_palindrom(s)
+    "oozoo"
+    """
+    out = [0, 1] #start, stop
+    prev_max_length = 1
+
+
+    for i in range(len(s) - 1):
+        j = 1
+        while (i - j >= 0) and (i + j < len(s)):
+            if s[i - j] == s[i + j]:
+                j += 1
+        max_length = max(prev_max_length, 2 * (j - 1) + 1)
+        if max_length > prev_max_length:
+            prev_max_length = max_length
+            out = [i - (j - 1), i + j]
+
+        # twin case
+        if s[i] == s[i + 1]:
+            j = 1
+            while (i - j > 0) and (i + 1 + j < len(s)):
+                if s[i - j] == s[i + 1 + j]:
+                    j += 1
+
+            max_length = max(prev_max_length, 2 * (j - 1) + 2)
+            if max_length > prev_max_length:
+                prev_max_length = max_length
+                out = [i - (j - 1), i + 1 + j]
+
+
+    return s[out[0]:out[1]]
--- LeetCode/test_longest_sub_palindrom.py
+++ LeetCode/test_longest_sub_palindrom.py
@@ -1,10 +1,17 @@
-from longest_sub_palindrom import longest_sub_palindrom
+from longest_sub_palindrom import naive_longest_sub_palindrom
 
 def test_SingleElement():
-    assert longest_sub_palindrom("a") == "a"
+    assert naive_longest_sub_palindrom("a") == "a"
+    assert naive_longest_sub_palindrom_enhanced("a") == "a"
 
-def test_AllSame():
-    assert longest_sub_palindrom("aaaaa") == "aaaaa"
+def test_AllSame_Odd():
+    assert naive_longest_sub_palindrom("aaaaa") == "aaaaa"
+    assert naive_longest_sub_palindrom_enhanced("aaaaa") == "aaaaa"
+
+
+def test_AllSame_Even():
+    assert naive_longest_sub_palindrom("aaaa") == "aaaa"
+    assert naive_longest_sub_palindrom_enhanced("aaaa") == "aaaa"
 
 
 # def test_AllDifferent():
@@ -13,4 +20,5 @@ def test_AllSame():
 # can be any of those letters (logically speaking)
 
 def test_hard_to_find():
-    assert longest_sub_palindrom("abccbdddbc") == "cbdddbc"
+    assert naive_longest_sub_palindrom("abccbdddbc") == "cbdddbc"
+    assert naive_longest_sub_palindrom_enhanced("abccbdddbc") == "cbdddbc"
