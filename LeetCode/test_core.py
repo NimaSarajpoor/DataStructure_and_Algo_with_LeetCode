7536ba5562b50b35add3b026336b50ca2e744c7b
ninimama <nimasarajpoor@gmail.com>
2022-07-08 19:23:41 -0600

    Fix test function

--- LeetCode/test_core.py
+++ LeetCode/test_core.py
@@ -1,4 +1,5 @@
 import core
+from core import Node, LinkedList
 import numpy as np
 from numpy import testing as npt
 
@@ -7,3 +8,22 @@ def test_Node():
     node = core.Node(val)
 
     npt.assert_equal(node.val, val)
+    assert node.next is None
+
+
+def test_LinkedList_root():
+    L = LinkedList()
+    assert L.root is None
+
+    val = np.random.rand()
+    L.insert_val(val)
+
+    npt.assert_equal(L.root.val, val)
+    assert L.root.next is None
+
+
+def test_LinkedList_list():
+    arr = np.random.rand(5)
+    L = LinkedList()
+    L.insert_from_list(arr)
+    npt.assert_almost_equal(arr[::-1], np.array(L.convert_to_lst()))
