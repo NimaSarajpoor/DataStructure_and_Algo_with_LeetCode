9e54c7c3f98c7003c05ce358f20d801887931e26
ninimama <nimasarajpoor@gmail.com>
2022-07-10 07:30:50 -0600

    Add function and test function

---
+++ LeetCode/reverse_int.py
@@ -0,0 +1,53 @@
+def reverse_int(x):
+    """
+    reverse x. If it is outside of a signed 32-bit integer range, then
+    return 0
+
+    Parameters
+    ----------
+    x: int
+        a signed 32-bit integer range
+
+    Returns
+    -------
+    out: int
+        reverse of x. If the output is outside of signed 32-bit range,
+        then return 0
+    """
+    out = None
+
+    if x == 0:
+        return x
+
+    max_val = 2147483647
+    min_val = -2147483648
+
+    if x < 0:
+        coef = -1
+        ref = str(abs(min_val))
+    else:
+        coef = 1
+        ref = str(abs(max_val))
+
+    x_str = str(abs(x))
+    x_str_reversed = x_str[::-1]
+    if len(x_str_reversed) > 10:
+        out =  0
+    elif len(x_str_reversed) < 10:
+        out = coef * int(x_str_reversed)
+    else:
+        # len(x_str_reversed) is exactly 10.
+        for i, s in enumerate(x_str_reversed):
+            if int(s) == ref[i]:
+                continue
+            elif int(s) < ref[i]:
+                out = coef * int(x_str_reversed)
+                break
+            else:
+                out = 0
+                break
+
+        if out is None:
+            out = coef * int(x_str_reversed)
+
+    return out
---
+++ LeetCode/test_reverse_int.py
@@ -0,0 +1,31 @@
+from reverse_int import reverse_int
+
+def test_positive_SingleDigit():
+    assert reverse_int(8) == 8
+
+def test_positive():
+    num = 1829723
+    assert reverse_int(num) == int(str(num)[::-1])
+
+def test_positive_max():
+    num = 2147483647
+    assert reverse_int(num) == int(str(num)[::-1])
+
+def test_positive_max_exceeded():
+    num = 2147483648
+    assert reverse_int(num) == 0
+
+def test_negative_SingleDigit():
+    assert reverse_int(-7) == -7
+
+def test_negative():
+    num = -1829723
+    assert reverse_int(num) == -1 * int(str(abs(num))[::-1])
+
+def test_negative_max():
+    num = -2147483648
+    assert reverse_int(num) == -1 * int(str(abs(num))[::-1])
+
+def test_negative_max_exceeded():
+    num = -2147483649
+    assert reverse_int(num) == 0
