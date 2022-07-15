53061e812ecfa27a3348db43f53d0e42151c4a6a
ninimama <nimasarajpoor@gmail.com>
2022-07-14 18:06:59 -0600

    add a few more functions and update meta

---
+++ LeetCode/letter_combinations.py
@@ -0,0 +1,54 @@
+# link: https://leetcode.com/submissions/detail/746783399/
+def letter_combinations(digits):
+    """
+    for a string consisting of digits chosen from 2 to 9 (repeatition allowed),
+    this function finds all possible letter combinations that the number could
+    represent according telephone string-to-number encoder.
+
+    Example:
+    letterCombinations("23"):
+    ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+    (2 is "abc", 3 represents "def")
+
+    Parameters
+    ----------
+    digits : str
+        a string of digits, where each digit can be any number from 2(inclusive)
+        to 9(inclusive)
+
+    Returns
+    -------
+    lst : List
+        a list of strings where each string can be represnted by `digits`
+    """
+    def letter_combinations_helper(digits, lst, mapper):
+        if len(digits) == 0:
+            return lst
+        else:
+            d = digits[-1]
+            if len(lst) == 0:
+                lst = [""]
+
+            lst_new = []
+            for item in lst:
+                for s in mapper[d]:
+                    lst_new.append(s + item)
+
+        return letter_combinations_helper(digits[:-1], lst_new, mapper)
+
+
+    mapper = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    }
+    lst = letter_combinations_helper(digits, [], mapper)
+
+    return lst
+
+# cleaner?
---
+++ LeetCode/three_sum_closest.py
@@ -0,0 +1,49 @@
+# link: https://leetcode.com/problems/3sum-closest/
+def three_sum_closest(nums, target):
+    """
+    Given an integer array nums of length n and an integer target, it will find
+    three integers in nums such that the sum is closest to target. It returns the
+    sum of the three integers.
+
+    Parameters
+    ----------
+    nums : List
+        A list of interger values
+
+    target : int
+        An integet
+
+    Returns
+    -------
+    out : int
+        The closest possible value to target that can be sum of three elements of
+        `nums`
+    """
+    nums.sort()
+
+    closest_so_far = float("inf")
+    out = float("inf")
+
+    for idx, v in enumerate(nums[:-2]):
+        i = idx + 1
+        j = len(nums) - 1
+
+        for _ in range(len(nums)): # upper bound for number of iterations
+            if i >= j:
+                break
+
+            s = nums[i] + nums[j] + v
+            net_val = s - target
+            if abs(net_val) < closest_so_far:
+                closest_so_far = abs(net_val)
+                out =  s
+
+            if net_val == 0:
+                return out
+
+            if net_val > 0:
+                j -= 1
+            else:
+                i += 1
+
+    return out
